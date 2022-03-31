f733ec7a0a011afc5941653874467323ff23acf6
JC Ho <JC.Ho2@nielsen.com>
2022-03-31 08:46:18 +0800

    Add varTest

---
+++ varTest/go.mod
@@ -0,0 +1,3 @@
+module varTest
+
+go 1.18
---
+++ varTest/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var a = "Hello"
+var i = 123
+
+func main() {
+	fmt.Println("================================")
+	b := 10
+	fmt.Println("a =>", &a, a, "b =>", &b, b)
+
+	a, b := 5, 20
+	fmt.Println("a =>", &a, a, "b =>", &b, b)
+	fmt.Println("================================")
+
+	fmt.Println("i =>", &i, i)
+	i = 123456
+	fmt.Println("i =>", &i, i)
+
+	i, j := 123, 456
+	fmt.Println("i =>", &i, i, "j =>", &j, j)
+	fmt.Println("================================")
+
+	const w = "Hello, world"
+	fmt.Println("w =", w)
+	fmt.Println("================================")
+	fmt.Println("a is", reflect.TypeOf(a))
+	fmt.Println("b is", reflect.TypeOf(b))
+	fmt.Printf("i is %T\n", i)
+	fmt.Println("w is", reflect.TypeOf(w))
+	fmt.Println("================================")
+}
