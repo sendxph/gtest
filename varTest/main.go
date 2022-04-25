a9b676043aab6e42b05d790c8755c0b6d23319a5
JC Ho <JC.Ho2@nielsen.com>
2022-04-25 14:41:33 +0800

    Update varTest

--- varTest/main.go
+++ varTest/main.go
@@ -44,4 +44,24 @@ func main() {
 	fmt.Printf("%+v\n", Name{})
 	fmt.Printf("%#v\n", Name{})
 	fmt.Println("================================")
+
+	arr := [5]int{1, 2, 3, 4, 5}
+	//fmt.Println("arr =>", &arr, arr)
+	fmt.Println("arr[4] =", arr[4])
+	fmt.Printf("arr => %p, %#v\n", &arr, arr)
+	fmt.Printf("arr is %T\n", arr) //same as fmt.Println("ar is", reflect.TypeOf(ar))
+	s := []int{6, 7, 8, 9, 10}
+	fmt.Printf("s => %p, %#v\n", &s, s)
+	fmt.Printf("s is %T\n", s)
+	s2 := make([]int, 5, 10)
+	fmt.Printf("s2 => %p, %v\n", &s2, s2)
+	fmt.Printf("s2 is %T\n", s2)
+	s3 := arr[1:3:4]
+	fmt.Printf("s3 => %p, %#v\n", &s3, s3)
+	fmt.Printf("s3 is %T\n", s3)
+	fmt.Println("s3 len =", len(s3))
+	fmt.Println("s3 cap =", cap(s3))
+	fmt.Println("s3[0] =", s3[0])
+	fmt.Println("s3[1] =", s3[1])
+	fmt.Println("================================")
 }
